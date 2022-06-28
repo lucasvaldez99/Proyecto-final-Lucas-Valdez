b906057d2942fb3f314bef049aba30118170c78a
jorge valdez <lucasvaldez99@gmail.com>
2022-06-28 10:08:40 -0300

    formulario con boostrap

---
+++ js/main.js
@@ -0,0 +1,17 @@
+  //Boostrap formulario
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+    }, false)
+    })
+    
+    //Fin de boostrap
+    
